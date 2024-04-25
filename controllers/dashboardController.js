3a5e62037a9a5ca846e26842ff3bd8a9973c14f0
Rayan Cherifi <rayan.cherifi.b3@gmail.com>
2024-04-25 18:17:12 +0200

    settings

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,14 +1,20 @@
 const axios = require("axios");
+const WeatherSettings = require("../models/Settings"); // Importez le modèle de paramètres météo
 
 const dashboardView = async (req, res) => {
   try {
-    // Effectuer une requête à l'API météo avec la clé d'API
+    // Récupérer les préférences météo de l'utilisateur à partir de la base de données
+    const weatherSettings = await WeatherSettings.findOne({ user: req.user._id });
+
+    // Personnaliser la requête à l'API météo en utilisant les préférences météo de l'utilisateur
     const response = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
       params: {
-        q: "Paris,fr", // Remplacez "Paris,fr" par la ville de votre choix
+        q: weatherSettings.location, // Utilisez l'emplacement météo préféré de l'utilisateur
         appid: "119fd7f95b40a15f2c35d190ea9b6df6", // Remplacez "VOTRE_CLE_API" par votre clé d'API
+        units: weatherSettings.temperatureUnit // Utilisez l'unité de température préférée de l'utilisateur
       },
     });
+
     // Rendre la vue avec les données de météo et les données de l'utilisateur
     res.render("dashboard", { user: req.user, weatherData: response.data });
   } catch (error) {
--- routes/login.js
+++ routes/login.js
@@ -1,25 +1,18 @@
 const express = require("express");
-
-const {
-  registerView,
-  loginView,
-  registerUser,
-  loginUser,
-  logoutView,
-} = require("../controllers/loginController");
+const router = express.Router();
+const { registerView, loginView, registerUser, loginUser, logoutView } = require("../controllers/loginController");
 const { dashboardView } = require("../controllers/dashboardController");
 const { protectRoute } = require("../auth/protect");
-
-const { test } = require("../controllers/testController")
-
-const router = express.Router();
+const { settingsView, updateSettings } = require("../controllers/settingsController"); // Importez le contrôleur des paramètres météo
 
 router.get("/register", registerView);
 router.get("/login", loginView);
 router.get("/logout", logoutView);
-//Dashboard
 router.get("/dashboard", protectRoute, dashboardView);
 router.get("/", protectRoute, dashboardView);
+
+router.get("/settings", protectRoute, settingsView); // Route pour afficher la page de paramètres météo
+router.post("/settings", protectRoute, updateSettings); // Route pour gérer la soumission du formulaire de paramètres météo
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
